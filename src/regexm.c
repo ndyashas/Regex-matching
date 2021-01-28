22a3e0cc7618645ea0e1607c9b7e3159af403fd3
Yashas ND <ndyashas@gmail.com>
2021-01-29 02:42:15 +0530

    Added init and release functions to NFA_STATE struct.

--- src/regexm.c
+++ src/regexm.c
@@ -11,11 +11,28 @@ void release_nfa(NFA **nfa)
 }
 
 void init_nfa_state(NFA_STATE **nfa_state)
-{  
+{
+	*nfa_state = (NFA_STATE *)malloc(sizeof(NFA_STATE));
+	(*nfa_state)->next_states = NULL;
+	(*nfa_state)->prev_states = NULL;
+	(*nfa_state)->number_of_next_states = 0;
+	(*nfa_state)->number_of_prev_states = 0;
+	(*nfa_state)->nfa_state_char = 0;
+	(*nfa_state)->end_state = 0;
 }
 
 void release_nfa_state(NFA_STATE **nfa_state)
 {
+	if (nfa_state == NULL || *nfa_state == NULL)
+		return;
+
+	if ((*nfa_state)->next_states != NULL)
+		free((*nfa_state)->next_states);
+
+	if ((*nfa_state)->prev_states != NULL)
+		free((*nfa_state)->prev_states);
+
+	free(*nfa_state);
 }
 
 int check_match(NFA **nfa, char *test_string)
